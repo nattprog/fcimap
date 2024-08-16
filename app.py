b21d5c8710591b55d4e052b36c0c6730fa4eaff8
Nathaniel Bong <npartotg7@gmail.com>
2024-08-16 10:35:04 +0800

    Basic index page

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
