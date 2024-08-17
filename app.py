1b21cfe161171c198a50e17c8add9118017a1626
Nathaniel Bong <npartotg7@gmail.com>
2024-08-18 01:41:45 +0800

    added colours on the current page, created current page argument to show which page we are on

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/index/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", CurrentPage="index")
 
 @app.route("/map/")
 def map():
-    return render_template("mmu.html")
+    return render_template("mmu.html", CurrentPage="map")
 
 if __name__ == "__main__":
     app.run(debug=True)
