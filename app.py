e890f0f9d62b6cbe8ef43ff28038861d560031b7
Nathaniel Bong <npartotg7@gmail.com>
2024-08-22 07:26:45 +0800

    Created a database for rooms and added block, floor and room numbers to it.

--- app.py
+++ app.py
@@ -34,4 +34,6 @@ def account():
     return render_template("account.html", ActivePage = "account")
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
