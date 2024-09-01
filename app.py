a3046ccdc3d53a78de711976ef391d3a35e1a361
Nathaniel Bong <npartotg7@gmail.com>
2024-09-02 03:32:14 +0800

    Added basic functional form/radio button for occupied and empty

--- app.py
+++ app.py
@@ -12,13 +12,13 @@ search = None
 class fci_room(db.Model):
         __tablename__ = "fci_room"
         id = db.Column(db.Integer, primary_key=True, nullable=False)
-        room_name = db.Column(db.String(50), nullable=False)
+        room_name = db.Column(db.String(50), nullable=False )
         room_block = db.Column(db.String(1), nullable=False)
         room_floor = db.Column(db.Integer, nullable=False)
         room_number = db.Column(db.Integer, nullable=False)
-        def __repr__(self, id, room_code, room_block, room_floor, room_number):
+        def __repr__(self, id, room_name, room_block, room_floor, room_number):
             self.id = id
-            self.room_code = room_code
+            self.room_name = room_name
             self.room_block = room_block
             self.room_floor = room_floor
             self.room_number = room_number
@@ -39,7 +39,7 @@ class User(db.Model):
     def __repr__(self):
         return f'<User {self.username}>'
 
-# -------------------------------------------------------  
+# -------------------------------------------------------
 
 @app.route("/")
 def redirect_home():
@@ -54,30 +54,47 @@ def home(floor):
             return redirect(f"/search/{search}")
     return render_template("index.html", ActivePage="index", ActiveFloor = floor)
 
-@app.route("/roompage/<room_code>", methods=["GET", "POST"])
-def room_page(room_code):
-    room = db.session.execute(db.select(fci_room).filter_by(room_code = room_code)).scalar()
+@app.route("/roompage/<room_name>", methods=["GET", "POST"])
+def room_page(room_name):
+    room = db.session.execute(db.select(fci_room).filter_by(room_name = room_name)).scalar()
+    search = None
+    if request.method == "POST":
+        try:
+            request.form["search"]
+        except:
+            pass
+        else:
+            search = request.form["search"]
+            return redirect(f"/search/{search}")
+        try:
+            request.form["room_status"]
+        except:
+            pass
+        else:
+            room_status = request.form["room_status"]
+            session["room_status"] = room_status
+    
+    return render_template("roompage.html", room_name = room.room_name, room_block = room.room_block, room_floor = room.room_floor, room_number = room.room_number, room_status = session["room_status"])
+    
+
+@app.route("/account/", methods=["GET", "POST"])
+def account():
     search = None
     if request.method == "POST":
         search = request.form["search"]
         if search:
             return redirect(f"/search/{search}")
-    return render_template("roompage.html", room_code = room.room_code, room_block = room.room_block, room_floor = room.room_floor, room_number = room.room_number)
-    
-
-@app.route("/account/", methods=["GET", "POST"])
-def account():
     return render_template("account.html", ActivePage = "account")
 
 @app.route("/search/<search>", methods=["GET", "POST"])
 def search(search):
     session["search"] = search
-    search_results = db.session.execute(db.select(fci_room).filter_by(room_code = search)).all()
+    search_results = db.session.execute(db.select(fci_room).filter_by(room_name = search)).all()
 
     results_list = []
     for i in range(len(search_results)):
         for ii in range(len(search_results[i])):
-            results_list.append(search_results[i][ii].room_code)
+            results_list.append(search_results[i][ii].room_name)
     
     if request.method == "POST":
         search = request.form["search"]
