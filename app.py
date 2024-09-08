3a866516e16036ddf7d12e7db65953780ce07b2d
Nathaniel Bong <npartotg7@gmail.com>
2024-09-09 01:11:47 +0800

    minor fixes

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def user_input_new_delete_old_schedule_decoder(schedule_input):
     schedule_input = schedule_input.replace("\n", " ")
 
     dates = r"(January\s*\d{1,2},\s*\d{4})|(February\s*\d{1,2},\s*\d{4})|(March\s*\d{1,2},\s*\d{4})|(April\s*\d{1,2},\s*\d{4})|(May\s*\d{1,2},\s*\d{4})|(June\s*\d{1,2},\s*\d{4})|(July\s*\d{1,2},\s*\d{4})|(August\s*\d{1,2},\s*\d{4})|(September\s*\d{1,2},\s*\d{4})|(October\s*\d{1,2},\s*\d{4})|(November\s*\d{1,2},\s*\d{4})|(December\s*\d{1,2},\s*\d{4})"
-    times = r"(\d{1,2}:\d{2}[AaPp][Mm])\s*-\s*(\d{1,2}:\d{2}[AaPp][Mm])\s*([A-Za-z]{4}\d{4})\s*:\s*([A-Za-z]*\d*)\s*-\s*\w*\s*(\(\w*\))"
+    times = r"(\d{1,2}:\d{2}[AaPp][Mm])\s*-\s*(\d{1,2}:\d{2}[AaPp][Mm])\s*([A-Za-z]{4}\d{4})\s*:\s*([A-Za-z]*\d*)\s*-\s*\w*\s*\((\w*)\)"
     # regex string to find in the input. 
 
     # months = string to match the dates
@@ -94,18 +94,6 @@ def room_status_func(room_status):
         room_status = "Occupied"
     return room_status, room_status_modifier
 
-def room_schedule_display_func(fci_room_id):
-    room_obj = db.session.execute(db.select(class_availability_schedule).filter_by(fci_room_id = fci_room_id)).all()
-
-    schedule_list = []
-    for i in range(len(room_obj)):
-        for ii in range(len(room_obj[i])):
-            schedule_list.append(room_obj[i][ii])
-    
-    return schedule_list
-
-
-
 # Database for block, floor and number of rooms.
 class fci_room(db.Model):
     __tablename__ = "fci_room"
@@ -130,8 +118,8 @@ class class_availability_schedule(db.Model):
     __tablename__ = "class_availability_schedule"
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     fci_room_id = db.Column(db.Integer, db.ForeignKey("fci_room.id"), nullable=False)
-    epoch_class_start = db.Column(db.String(50), nullable=False)
-    epoch_class_end = db.Column(db.String(50), nullable=False)
+    epoch_class_start = db.Column(db.Float(50), nullable=False)
+    epoch_class_end = db.Column(db.Float(50), nullable=False)
     class_subject_code = db.Column(db.String(50), nullable=False)
     class_section = db.Column(db.String(50), nullable=False)
     def __repr__(self, id, fci_room_id, epoch_class_start, epoch_class_end, class_subject_code, class_section):
@@ -199,10 +187,9 @@ def room_page(room_name):
             db.session.commit()
 
     room_status, room_status_modifier = room_status_func(room_status=room.room_status)
-    schedule_list = []
 
+    # room availability schedule
     room_obj = db.session.execute(db.select(class_availability_schedule).filter_by(fci_room_id = room.room_name)).all()
-
     schedule_list = []
     for i in range(len(room_obj)):
         for ii in range(len(room_obj[i])):
